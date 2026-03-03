a33a22b6d73c6e9038737e29064517d379d47908
Santos, Thiago R <thiago_thg@outlook.com>
2026-03-02 21:47:49 -0300

    feat: added fastify

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import Fastify from "fastify";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", async function handler() {
+  return { hello: "world" };
+});
+
+try {
+  await fastify.listen({ port: Number(process.env.PORT) || 3000 });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
