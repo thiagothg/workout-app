d193f174156f323aa5fe95d2ac6f87db91f44432
Santos, Thiago R <thiago_thg@outlook.com>
2026-03-10 18:40:48 -0300

    fix: update host

--- src/index.ts
+++ src/index.ts
@@ -146,7 +146,7 @@ app.route({
 });
 
 try {
-  await app.listen({ port: Number(process.env.PORT) || 3000 });
+  await app.listen({ host: "0.0.0.0", port: Number(process.env.PORT) || 3000 });
 } catch (err) {
   app.log.error(err);
   process.exit(1);
